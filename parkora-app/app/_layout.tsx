7c1395b9b15137ac0829397abde7b7ea852e9a13
MohammedTK <monw2021@gmail.com>
2026-04-18 14:31:27 +0100

    add logo

--- parkora-app/app/_layout.tsx
+++ parkora-app/app/_layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="(parking)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       </Stack>
     </SafeAreaProvider>
   );
